8e50a7ce444713c31bd020717c17735b5d336759
hudsonmp <hudsonmitchellpullman@gmail.com>
2025-01-12 07:43:00 -0800

    Began linear analysis. Model is not precise, I need to fine tune it and review my notes.

---
+++ Variables.md
@@ -0,0 +1,4 @@
+FirstServePointsWon
+ReturnGamesWon
+ServiceGamesWon
+BreakPointsSaved
--- script.py
+++ script.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import SGDRegressor
+import numpy as np
 
 # load and investigate the data here:
 
@@ -21,18 +22,35 @@ print(tennis.describe(include='all'))
 
 
 # perform exploratory analysis here:
+plt.scatter(tennis['Ranking'], tennis['DoubleFaults'])
+plt.xlabel('Ranking')
+plt.ylabel('DoubleFaults')
+plt.title('Double Faults vs. Ranking')
+#plt.show()
+#plt.clf()
+tennis['total_matches'] = tennis['Wins'] + tennis['Losses']
+tennis['win_percentage']  = np.where(tennis['total_matches'] > 0, tennis['Wins'] / tennis['total_matches'], 0)
+'''for column in tennis.columns:
+    #print(tennis[column].dtype)
+    if tennis[column].dtype != 'object':
+        plt.scatter(tennis['win_percentage'], tennis[column])
+        plt.xlabel('Win Percentage')
+        plt.ylabel('{}'.format(column))
+        plt.title('{} vs. Win Percentage'.format(column))
+        plt.show()
+        plt.clf()
+    else:
+        pass'''
+from scipy.stats import pearsonr
+from scipy import stats
+pearson_score, x= stats.pearsonr(tennis['Ranking'], tennis.win_percentage)
+print(tennis.info())
+print(pearson_score)
+pearson, f = stats.pearsonr(tennis['total_matches'], tennis['win_percentage'])
+print(pearson)
 
-
-
-
-
-
-
-
-
-
-
-
+pearsonr, x = stats.pearsonr(tennis['win_percentage'], tennis['FirstServePointsWon'])
+print(pearsonr)
 
 
 
@@ -44,7 +62,13 @@ print(tennis.describe(include='all'))
 
 
 ## perform single feature linear regressions here:
-
+fpw = np.array(tennis['Wins`']).reshape(-1, 1)
+ranking = np.array(tennis['Ranking']).reshape(-1, 1)
+fpw_train, fpw_test, ranking_train, ranking_test = train_test_split(fpw, ranking, test_size=0.2)
+regr = LinearRegression()
+regr.fit(fpw_train, ranking_train)
+score_fpw_ranking = regr.score(fpw_test, ranking_test)
+print('\n', score_fpw_ranking)
 
 
 
